f1b6e157bf910f02d0112fbd03c6387c6ea68cca
soilflux <lightningbagel1@gmail.com>
2026-02-15 20:48:30 -0500

    fix barrens w shiftChecks as well

--- hints.js
+++ hints.js
@@ -270,7 +270,7 @@ function wothAndBarrenProcessing() {
           const isNotBoss = !(dungeon?.bossCheck && bossStrings.includes(loc));
           const isDisplayed = document.getElementById(loc).style.display != "none";
 
-          if (isUnknown && isNotHint && isNotBoss && isDisplayed) {
+          if (isUnknown && isNotHint && isNotBoss && (isDisplayed || document.getElementById("shiftChecks").value == "YES")) {
             document.getElementById("text_" + loc).dispatchEvent(new Event('mousedown'));
           }
         });
